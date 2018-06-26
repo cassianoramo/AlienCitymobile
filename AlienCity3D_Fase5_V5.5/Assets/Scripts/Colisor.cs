b7019c175e63a29a31cda66fa8fe56b93777fdb3
CassianoRamos <30438171+CassianoRamos@users.noreply.github.com>
2018-06-25 23:19:45 -0300

    Animações dos obstaculosv1.0

---
+++ AlienCity3D_Fase5_V5.5/Assets/Scripts/Colisor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Colisor : MonoBehaviour {
+
+	void  OnTriggerEnter (Collider other){
+		if  (other.gameObject.CompareTag ( "Obstaculo")) {
+			string currentScene = SceneManager.GetActiveScene ().name;
+			SceneManager.LoadScene (currentScene);
+		}
+	}
+}
