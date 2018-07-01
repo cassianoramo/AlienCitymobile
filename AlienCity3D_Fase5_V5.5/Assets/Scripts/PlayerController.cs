2a6583d0c449640eb609492c7e79d48401e22998
CassianoRamos <30438171+CassianoRamos@users.noreply.github.com>
2018-07-01 14:36:51 -0300

    AT1 rotation v2.0

--- AlienCity3D_Fase5_V5.5/Assets/Scripts/PlayerController.cs
+++ AlienCity3D_Fase5_V5.5/Assets/Scripts/PlayerController.cs
@@ -10,9 +10,10 @@ public class PlayerController : MonoBehaviour {
 	CharacterController cc;
 	private Animator anim;
 	protected Vector3 gravidade = Vector3.zero;
-	protected Vector3 move = Vector3.zero;
+	protected Vector3 move1 = Vector3.zero;
 	private bool jump = false;
 	private float h;
+	private float v;
 
 	void Start()
 	{
@@ -24,9 +25,8 @@ public class PlayerController : MonoBehaviour {
 	void Update()
 	{
 		Move ();
-		/*Vector3 move = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
-		transform.Rotate (new Vector3 (0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
-		*/
+		//Vector3 move = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
+		//transform.Rotate (new Vector3 (0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
 		if (!cc.isGrounded) {
 			gravidade += Physics.gravity * Time.deltaTime;
 		} 
@@ -66,15 +66,25 @@ public class PlayerController : MonoBehaviour {
 	}
 	void Move(){
 		h = CrossPlatformInputManager.GetAxis ("Horizontall");
+		v = CrossPlatformInputManager.GetAxis ("Verticall");
 		if(Input.GetKey("right") || Input.GetKey("left") || h > 0){
 			transform.Rotate (new Vector3 (0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
 		}
 		if (h > 0 || h < 0) {
 			transform.Rotate (new Vector3 (0, h * RotationSpeed * Time.deltaTime, 0));
 		}
-		Vector3 move = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
-		move += gravidade;
-		cc.Move (move* Time.deltaTime);
+		if (Input.GetKey ("up") || Input.GetKey ("down") || v > 0) {
+			Vector3 move = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
+			move += gravidade;
+			cc.Move (move * Time.deltaTime);
+		}
+		if (v < 0 || v > 0) {
+			Vector3 move = v * transform.TransformDirection (Vector3.forward) * MoveSpeed;
+			move += gravidade;
+			cc.Move (move * Time.deltaTime);
+		}
+		move1 += gravidade;
+		cc.Move (move1 * Time.deltaTime);
 	}
 }
 
