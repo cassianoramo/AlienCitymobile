bbdd965de7cdd713fcc93f73f86c0689a87cce19
CassianoRamos <30438171+CassianoRamos@users.noreply.github.com>
2018-07-09 20:57:32 -0300

    AT1 tentativa 4

--- AlienCity3D_Fase5_V5.5/Assets/Scripts/PlayerController.cs
+++ AlienCity3D_Fase5_V5.5/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
 [RequireComponent(typeof(CharacterController))]
-//[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
 
 	public float MoveSpeed;
@@ -11,56 +10,42 @@ public class PlayerController : MonoBehaviour {
 	CharacterController cc;
 	private Animator anim;
 	protected Vector3 gravidade = Vector3.zero;
-	protected Vector3 move1 = Vector3.zero;
-	//private bool jump = false;
-	private float h;
-	private float v;
-	//public Rigidbody player;
-	/*public Vector3 direcaopulo = new Vector3(0,1,0);
-	[Range(1,20)]
-	public float forcadopulo = 5.0f;
-	[Range(0.5f,10.0f)]
-	public float distanciadochao;
-	[Range(0.5f,5.0f)]
-	public float tempoporpulo = 1.5f;
-	public LayerMask layers = -1;
-	private bool estanochao, contar = false;
-	private float cronometro = 0;
-	private Rigidbody corporigido;*/
+	protected Vector3 move = Vector3.zero;
+	private bool jump = false;
+
+
 
 	void Start()
 	{
 		cc = GetComponent<CharacterController> ();
 		anim = GetComponent<Animator>();
-		//player = GetComponent<Rigidbody> ();
 		anim.SetTrigger("Parado");
-		//corporigido = GetComponent<Rigidbody> ();
 	}
 
 	void Update()
 	{
-		Move ();
-		Anima ();
-		if (!cc.isGrounded ) {
+		float h = CrossPlatformInputManager.GetAxis ("Horizontall");
+		float v = CrossPlatformInputManager.GetAxis ("Verticall");
+		Vector3 move = v * transform.TransformDirection (Vector3.forward) * MoveSpeed;
+		transform.Rotate (new Vector3 (0, h * RotationSpeed * Time.deltaTime, 0));
+
+		if (!cc.isGrounded) {
 			gravidade += Physics.gravity * Time.deltaTime;
-		} else {
+		} 
+		else 
+		{
 			gravidade = Vector3.zero;
+			if(jump)
+			{
+				gravidade.y = 6f;
+				jump = false;
+			}
 		}
-		/*estanochao = Physics.Linecast (transform.position, transform.position - Vector3.up * distanciadochao, layers);
-		if (Input.GetKeyDown (KeyCode.Space) && estanochao == true && contar == false) {
-			corporigido.AddForce (direcaopulo * forcadopulo, ForceMode.Impulse);
-			estanochao = false;
-			contar = true;
-		}
-		if (contar == true) {
-			cronometro += Time.deltaTime;
-		}
-		if (cronometro >= tempoporpulo) {
-			contar = false;
-			cronometro = 0;
-		}*/
+		move += gravidade;
+		cc.Move (move* Time.deltaTime);
+		Anima ();
 	}
-	 
+
 	void Anima()
 	{
 		if (!Input.anyKey)
@@ -72,48 +57,20 @@ public class PlayerController : MonoBehaviour {
 			if(Input.GetKeyDown("space"))
 			{
 				anim.SetTrigger("Pula");
-				//jump = true;
-		
+				jump = true;
 			}
-	else
+			else
 			{
 				anim.SetTrigger("Corre");
 			}
 		}
-	/*	if (jump) {
-			Jump ();
-		} */
 	}
-	void Move(){
-		h = CrossPlatformInputManager.GetAxis ("Horizontall");
-		v = CrossPlatformInputManager.GetAxis ("Verticall");
-		if(Input.GetKey("right") || Input.GetKey("left") || h > 0){
-			transform.Rotate (new Vector3 (0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
-		}
-		if (h > 0 || h < 0) {
-			transform.Rotate (new Vector3 (0, h * RotationSpeed * Time.deltaTime, 0));
-		}
-		if (Input.GetKey ("up") || Input.GetKey ("down") || v > 0) {
-			Vector3 move = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
-			move += gravidade;
-			cc.Move (move * Time.deltaTime);
-		}
-		if (v < 0 || v > 0) {
-			Vector3 move = v * transform.TransformDirection (Vector3.forward) * MoveSpeed;
-			move += gravidade;
-			cc.Move (move * Time.deltaTime);
-		}
-		move1 += gravidade;
-		cc.Move (move1 * Time.deltaTime);
+	public void Jump(){
+		jump = true;
+		anim.SetTrigger("Pula");
 	}
-	/*void Jump (){
-		if (cc.isGrounded) {
-			Debug.Log ("rxetcyo");
-			player.AddForce (Vector3.up * 2000 * Time.deltaTime);
-			jump = false;
-			Debug.Log ("rxe");
-		}
-	}*/
 }
+
+
 
 
